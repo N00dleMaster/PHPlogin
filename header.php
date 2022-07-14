2079d03f04fa08f1b8248f179740eec48f23a84c
N00dleMaster <ayaan.path@gmail.com>
2022-07-13 21:44:08 -0400

    Initial Commit

---
+++ header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>PHP Login System</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;600&family=Roboto+Condensed&family=Roboto+Mono&family=Roboto:wght@300;400;700&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="css/styles.css">
+    </head>
+    <body>
+        
+        <!-- NAVBAR -->
+        <nav>
+            <ul class="menu general">
+                <li><a href="index.php" id="logo">Login System</a></li>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="discover.php">Discover</a></li>
+                <li><a href="about.php">About Us</a></li>
+                <li><a href="signup.php">Sign Up</a></li>
+                <li><a href="login.php">Log In</a></li>
+            </ul>
+            <ul class="menu member">
+                <li><a href="#">Sign Up</a></li>
+                <li><a href="#">Log In</a></li>
+            </ul>
+        </nav>
---
+++ index.php
@@ -0,0 +1,36 @@
+<!-- See header.php for the opening nav bar and <head> tags -->
+
+        <?php include "header.php" ?>
+
+        <!-- MAIN CONTENT -->
+        <div class="wrapper">
+            <section class="index-intro">
+                <h1>This is an Instruction</h1>
+                <p>This is an important paragraph explaining the purpose of the website</p>
+            </section>
+
+            <section class="index-categories">
+                <h2>Some Basic Categories</h2>
+                <div class="index-categories-list">
+                    <div>
+                        <h3>Fun Stuff</h3>
+                    </div>
+                    <div>
+                        <h3>Exciting Stuff</h3>
+                    </div>
+                    <div>
+                        <h3>Stupid Stuff</h3>
+                    </div>
+                    <div>
+                        <h3>Serious Stuff</h3>
+                    </div>
+                </div>
+            </section>
+        </div>
+
+
+
+        
+        <script src="" async defer></script>
+    </body>
+</html>
